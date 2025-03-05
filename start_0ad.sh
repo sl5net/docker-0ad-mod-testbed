3ddc8fe86712fdcb089623d84dc608acf4b72f66
sl5net <sl5softwarelab@gmail.com>
2025-03-05 22:05:36 +0100

    use opengl

--- start_0ad.sh
+++ start_0ad.sh
@@ -4,10 +4,13 @@
 xhost +local:docker
 
 export XDG_RUNTIME_DIR=/run/user/$(id -u)
+# export SDL_VIDEODRIVER=x11
+export SDL_RENDER_DRIVER=opengl
 
 docker run --rm -it \
     -e DISPLAY="$DISPLAY" \
     -e SDL_VIDEODRIVER=x11 \
+    -e SDL_RENDER_DRIVER=opengl \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --device /dev/snd \
     -e PULSE_SERVER="unix:${XDG_RUNTIME_DIR}/pulse/native" \
@@ -15,6 +18,7 @@ docker run --rm -it \
     -e ALSA_PCM_NAME=default \
     --device /dev/dri \
     --group-add video \
+    --gpus all \
     -v "$(pwd)/0ad-extracted:/opt/0ad" \
     -v 0ad-config:/home/0aduser/.config/0ad \
     0ad
