0c34018dd5e5c73f5d111a58196d604f79eda39d
sl5net <sl5softwarelab@gmail.com>
2025-03-05 19:32:23 +0100

    cocker-entrypont.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,5 +7,19 @@ if [ -z "$DISPLAY" ]; then
   export DISPLAY=:0
 fi
 
-# Benutzer zum 0aduser wechseln
-exec su - 0aduser -c "/game/usr/bin/0ad -writableRoot -config=/home/0aduser/.config/0ad -mod=/home/0aduser/.local/share/0ad/mods"
+# Copy default config to volume if it is empty
+if [ ! -d "/var/0ad/config/0ad" ]; then
+  echo "Copying default config to volume"
+  mkdir -p /var/0ad/config/0ad
+  cp -r /home/0aduser/.config/0ad/* /var/0ad/config/0ad/
+fi
+
+# Copy default mods to volume if it is empty
+if [ ! -d "/var/0ad/mods/0ad" ]; then
+    echo "Copying default mods to volume"
+    mkdir -p /var/0ad/mods/0ad
+    cp -r /home/0aduser/.local/share/0ad/mods/* /var/0ad/mods/0ad/
+fi
+
+# Starte 0 A.D.
+/game/usr/bin/0ad -writableRoot -config=/var/0ad/config -mod=/var/0ad/mods
